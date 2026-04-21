ff8db0614e5b29909944eb11f0b4deef65716f25
Diego <dieguinsal.521@gmail.com>
2026-04-21 11:45:46 -0600

    Modificacion de  fondo 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import "../styles/hero.css";
-import hero from "../assets/hero1.png";
+import hero from "../assets/hero2.jpg";
 import useCountdown from "../hooks/useCountdown";
 import { config } from "../data/config";
 
