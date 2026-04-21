500b713520434c6cea68ef348ee74f5564749e3c
Diego <dieguinsal.521@gmail.com>
2026-04-21 10:41:44 -0600

    Modificacion de fondo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,19 +10,19 @@ export default function Hero() {
     <section className="hero-section">
       <div className="hero-wrapper">
         <img src={hero} alt="Invitación" className="hero-img" />
-        
         <div className="hero-overlay">
           <p className="hero-tag">MIS XV AÑOS</p>
-          <h3 className="hero-names">Stephany Guadalupe</h3>
-          <p className="hero-date">30 De Mayo Del 2026</p>
+          <h3 className="hero-names">Stephany <br/> Guadalupe</h3>
+          <p className="hero-date">30 • Mayo • 2026</p>
 
           <div className="hero-counter">
             <div className="counter-item"><span>{d}</span><small>Días</small></div>
-            <div className="counter-item"><span>{h}</span><small>Horas</small></div>
-            <div className="counter-item"><span>{m}</span><small>Minutos</small></div>
+            <div className="counter-item"><span>{h}</span><small>Hrs</small></div>
+            <div className="counter-item"><span>{m}</span><small>Min</small></div>
             <div className="counter-item"><span>{s}</span><small>Seg</small></div>
           </div>
         </div>
+        
       </div>
     </section>
   );
