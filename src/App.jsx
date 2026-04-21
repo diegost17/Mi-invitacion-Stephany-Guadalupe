9f4a3691c7c0149c296153a25b05203cd761520b
Diego <dieguinsal.521@gmail.com>
2026-04-21 12:31:44 -0600

    Posicionamiento de cancion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
 import EventInfo from "./components/EventInfo";
 import Gallery from "./components/Gallery";
+import AudioPlayer from "./components/AudioPlayer";
 
 function App() {
   return (
     <>
+      <AudioPlayer />
       <Hero />
       <EventInfo />
       <Gallery />
